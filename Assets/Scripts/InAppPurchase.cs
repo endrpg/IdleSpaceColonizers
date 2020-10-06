0a33be387e364a178b0a1dd8148f3325e83cef43
Kushal Agrawal <kushalagrawal890@gmail.com>
2020-10-06 13:56:00 +0530

    changes

---
+++ Assets/Scripts/InAppPurchase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+public class InAppPurchase : MonoBehaviour
+{
+    ClickManager clickManager;
+    public int gemstoGive = 100;
+    void Start()
+    {
+        clickManager = GameObject.Find("Play Area").GetComponent<ClickManager>();
+    }
+    public void OnPurchaseComplete(Product product)
+    {
+        clickManager.gems += gemstoGive;
+    }
+    public void OnPurchaseFailure(Product product,PurchaseFailureReason reason)
+    {
+        Debug.Log("Failure");
+    }
+}
