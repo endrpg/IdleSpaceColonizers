77955ad680e76619527abf60e4bbd6aaaaa66d47
Kushal Agrawal <kushalagrawal890@gmail.com>
2020-10-12 00:05:09 +0530

    Tutorial and some checks

---
+++ Assets/Scripts/DialogueDelievery/Dialogue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    [TextArea(10,20)]
+    public string[] sentences;
+    public Sprite[] images;
+    public Image Bg_Image; 
+    private int index;
+    public float typingSpeed = 0.02f;
+    public GameObject continueButton;
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+    IEnumerator Type()
+    {
+        if(images[index] != null)
+        {
+            Bg_Image.sprite = images[index];
+        }
+        foreach (var letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueDelievery/TutorialButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TutorialButton : MonoBehaviour
+{
+    Button me;
+    // Start is called before the first frame update
+    void Start()
+    {
+        me = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        me.onClick.RemoveAllListeners();
+        me.onClick.AddListener(delegate{TransportingToTutorial();});
+    }
+    void TransportingToTutorial()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+}
